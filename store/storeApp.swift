234f69ec3d8bc571e6fc3e1a6b020b79cb2e080c
Andres Duque <andresduke024@gmail.com>
2022-12-14 21:30:58 -0500

    Initial Commit

---
+++ store/storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  storeApp.swift
+//  store
+//
+//  Created by Andres Duque on 14/12/22.
+//
+
+import SwiftUI
+
+@main
+struct storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
